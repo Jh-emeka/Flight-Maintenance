a7b07a6574a5fc89eee564fd8fab347913ef6c5d
joshuahorsfall <emeka.horsfall@gmail.com>
2023-12-01 15:40:49 +0000

    Make some changes

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect, g
 from User import User
-from gevent.pywsgi import WSGIServer
-import os
 from config import app
 import bcrypt as bcrypt
 import sqlite3 as sqlite
